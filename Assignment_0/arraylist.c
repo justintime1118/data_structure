af45920aae03dff9757a215d58f88b584e328979
Jihyeong Yoo <jiyoo@c3r1s4.42seoul.kr>
2022-04-16 19:12:30 +0900

    implementation done

--- Assignment_0/arraylist.c
+++ Assignment_0/arraylist.c
@@ -2,6 +2,32 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int main(void)
+{
+	ArrayList *pList;
+	ArrayListNode node;
+
+	node.data = 1;
+	
+	pList = createArrayList(5);
+	
+	for (int i = 0; i < 6; i++)
+	{
+		addALElement(pList, i, node);
+		printf("length == %d\n", getArrayListLength(pList));
+		node.data++;
+	}
+	
+	if (isArrayListFull(pList) == TRUE)
+		printf("TRUE FULL\n");
+	removeALElement(pList, 3);
+	addALElement(pList, 1, node);
+	// deleteArrayList(pList);
+	// clearArrayList(pList);
+	// addALElement(pList, 0, node);
+	displayArrayList(pList);
+}
+
 ArrayList* createArrayList(int maxElementCount)
 {
 	ArrayList *pList = (ArrayList *)malloc(sizeof(ArrayList));
@@ -10,14 +36,22 @@ ArrayList* createArrayList(int maxElementCount)
 	pList->currentElementCount = 0;
 	pList->pElement = (ArrayListNode *)malloc(sizeof(ArrayListNode) * maxElementCount);
 	if (pList->pElement == 0)
-		perror(NULL);
+	{
+		printf("malloc failed\n");
+		return (NULL);
+	}
+	for (int i = 0; i < maxElementCount; i++)
+		pList->pElement[i].data = 0;
 	return (pList);
 }
 
-// 매개변수로 주어지는 pList가 NULL일 경우에 대해서 따로 처리를 해줘야할까?
-
 void deleteArrayList(ArrayList* pList)
 {
+	if (pList == NULL)
+	{
+		printf("the ArrayList is NULL\n");
+		return ;
+	}
 	pList->maxElementCount = 0;
 	pList->currentElementCount = 0;
 	free(pList->pElement);
@@ -27,22 +61,56 @@ void deleteArrayList(ArrayList* pList)
 
 int isArrayListFull(ArrayList* pList)
 {
+	if (pList == NULL)
+	{
+		printf("the ArrayList is NULL\n");
+		return (-1);
+	}
 	if (pList->currentElementCount == pList->maxElementCount)
 		return (TRUE);
 	else
 		return (FALSE);
 }
 
-// position이 index 번호로 주어진다고 가정.
+// position이 index 번호로 주어진다고 가정
+// 여기서 자바에서 사용하는 ArrayList처럼 만들어야 할지, 아니면 C의 primitive 배열처럼 만들어야 할지 고민이 됐다
+// 고민 좀 하다가 둘이 섞은 방식으로 하기로 함
 int addALElement(ArrayList* pList, int position, ArrayListNode element) 
 {
-	// 기존 배열이 꽉 차있었는지 등등에 따라서 달라짐
-	// currentElementCount에도 반영해줘야 함
+	int	value = element.data;
+
+	if (pList == NULL || pList->pElement == NULL)
+	{
+		printf("ArrayList is invalid\n");
+		return (-1);
+	}
+	if (isArrayListFull(pList) == TRUE)
+	{
+		printf("the ArrayList is full\n");
+		return (-1);
+	}
+	if (position < 0 || pList->currentElementCount < position)
+	{
+		printf("position out of bound\n");
+		return (-1);
+	}
+	if (position <= pList->currentElementCount - 1)
+	{
+		for (int i = pList->currentElementCount - 1; i >= position; i--)
+			pList->pElement[i + 1].data = pList->pElement[i].data;
+	}
+	pList->pElement[position].data = value;
+	pList->currentElementCount++;
+	return (position);
 }
 
 int removeALElement(ArrayList* pList, int position)
 {
-	if (0 <= position && position < pList->currentElementCount)
+	if (pList == NULL || pList->pElement == 0)
+		printf("ArrayList is invalid\n");
+	else if (pList->currentElementCount == 0)
+		printf("the ArrayList is empty\n");
+	else if (0 <= position && position < pList->currentElementCount)
 	{
 		for (int i = position; i < pList->currentElementCount - 1; i++)
 			pList->pElement[i].data = pList->pElement[i + 1].data;
@@ -50,43 +118,58 @@ int removeALElement(ArrayList* pList, int position)
 		pList->pElement[pList->currentElementCount].data = 0;
 		return (position);
 	}
-	else if (pList->currentElementCount == 0)
-		printf("the ArrayList is empty\n");
 	return (-1);
 }
 
 ArrayListNode* getALElement(ArrayList* pList, int position)
 {
-	if (0 <= position && position < pList->maxElementCount)
-		return (&(pList->pElement[position]));
-	else
+	if (pList == NULL || pList->pElement == 0)
+	{
+		printf("ArrayList is invalid\n");
 		return (NULL);
+	}
+	if (position < 0 || pList->currentElementCount <= position)
+	{
+		printf("position out of bound\n");
+		return (NULL);
+	}
+	return (&(pList->pElement[position]));
 }
 
 void displayArrayList(ArrayList* pList)
 {
+	if (pList == NULL || pList->pElement == 0)
+	{
+		printf("ArrayList is invalid\n");
+		return ;
+	}
+	if (pList->currentElementCount == 0)
+	{
+		printf("the ArrayList is empty\n");
+		return ;
+	}
 	for (int i = 0; i < pList->currentElementCount; i++)
-		printf("%d ", pList->pElement[i]);
+		printf("%d ", pList->pElement[i].data);
 }
 
 void clearArrayList(ArrayList* pList)
 {
+	if (pList == NULL || pList->pElement == 0)
+	{
+		printf("ArrayList is invalid\n");
+		return ;
+	}
 	for (int i = 0; i < pList->currentElementCount; i++)
 		pList->pElement[i].data = 0;
+	pList->currentElementCount = 0;
 }
 
 int getArrayListLength(ArrayList* pList)
 {
+	if (pList == NULL || pList->pElement == 0)
+	{
+		printf("ArrayList is invalid\n");
+		return (-1);
+	}
 	return (pList->currentElementCount);
 }
-
-/*
-선형 자료구조 - 배열, 리스트
-비선형 자료구조 - 트리, 그래프
-
-시간복잡도
-insert - 배열 n, 리스트 1
-값 추출 - 배열 1, 리스트 n
-
-두 자료구조 간의 차이와 장단점, 어떤 경우에 어떤 자료구조를 선택하는 것이 좋은지에 대해서 잘 생각해 볼 것
-*/
--- Assignment_0/linkedlist.c
+++ Assignment_0/linkedlist.c
@@ -1,57 +1,166 @@
-#include "linkedlist.h"
-#include <stdlib.h>
-#include <stdio.h>
+#include	"linkedlist.h"
+#include	<stdio.h>
+#include	<stdlib.h>
 
-// 헤더는 그냥 비어있고 그 다음 노드부터가 리스트의 시작으로 한다.
+
+int	main(void)
+{
+	LinkedList *alist;
+	ListNode *element;
+	ListNode *get;
+	int	i;
+
+	alist = createLinkedList();
+	element = (ListNode *)malloc(sizeof(ListNode));
+	element->data = 33;
+	element->pLink = NULL;
+	for (int i = 0; i < 5; i++)
+	{
+		addLLElement(alist, i, *element);
+		element->data++;
+	}
+	for (int i = 0; i < getLinkedListLength(alist); i++)
+		printf("%d ", getLLElement(alist, i)->data);
+	printf("\n");
+	//removeLLElement(alist, 2);
+	deleteLinkedList(alist);
+	// for (int i = 0; i < getLinkedListLength(alist); i++)
+	// 	printf("%d ", getLLElement(alist, i)->data);
+	// printf("\n");
+	// printf("size == %d\n", getLinkedListLength(alist));
+	system("leaks a.out");
+	return(0);
+}
+
+/* 연결리스트 생성 */
 LinkedList* createLinkedList()
 {
 	LinkedList *pList;
 
 	pList = (LinkedList *)malloc(sizeof(LinkedList));
 	if (pList == 0)
-		perror(NULL);
+	{
+		printf("malloc failed\n");
+		return (NULL);
+	}
 	pList->currentElementCount = 0;
 	pList->headerNode.data = 0;
 	pList->headerNode.pLink = NULL;
 	return (pList);
 }
 
-// position이 만약 0이면 헤더를 가리키는 것으로 본다. 1이면 첫번째 노드
+/* 연결리스트 원소 추가 */
 int addLLElement(LinkedList* pList, int position, ListNode element)
 {
-	ListNode *tmp = &(pList->headerNode);
+	ListNode *tmp;
+	ListNode *new;
 
-	if (position > pList->currentElementCount)
+	if (position < 0 || position > pList->currentElementCount)
 	{
-		printf("position out of bound\n");
+		printf("1position out of bound\n");
 		return (-1);
+	}
+	new = (ListNode *)malloc(sizeof(ListNode));
+	new->data = element.data;
+	new->pLink = NULL;
+	if (position == 0)
+	{
+		tmp = (pList->headerNode).pLink;
+		(pList->headerNode).pLink = new;
 	}
 	else
 	{
-		// 아직 미완성임. 여기서부터 이어서 가자
-		for (int i = 0; i < position; i++)
+		tmp = getLLElement(pList, position - 1);
+		element.pLink = tmp->pLink;
+		tmp->pLink = new;
+	}
+	pList->currentElementCount++;
+	return (pList->currentElementCount);
+}
+
+/* 연결리스트 원소 삭제 */
+int removeLLElement(LinkedList* pList, int position)
+{
+	ListNode *remove;
+	ListNode *tmp;
+
+	if (position < 0 || position > pList->currentElementCount)
+	{
+		printf("2position is out of bound\n");
+		return (-1);
+	}
+	tmp = getLLElement(pList, position - 1);
+	remove = tmp->pLink;
+	tmp->pLink = remove->pLink;
+	free(remove);
+	pList->currentElementCount--;
+	return (pList->currentElementCount);
+}
+
+/* 연결리스트 원소 반환 */ /////
+ListNode* getLLElement(LinkedList* pList, int position)
+{
+	ListNode *tmp;
+	int	cnt;
+
+	if (pList == NULL)
+		printf("the Array List is NULL\n");
+	else if (pList->currentElementCount == 0)
+		printf("LinkedList is empty\n");
+	else if (position < 0 || position >= pList->currentElementCount)
+		printf("3position is out of bound\n");
+  	else
+	{
+		tmp = (pList->headerNode).pLink; // 첫번째 노드
+		for (cnt = 0; cnt < position; cnt++) // position만큼 뒤로 이동
 			tmp = tmp->pLink;
-		
+		return (tmp);
+	}
+	return (NULL);
+}
+
+/* 연결리스트 초기화 */
+void clearLinkedList(LinkedList* pList)
+{
+	ListNode *clear;
+
+	if (pList == NULL)
+		printf("the Array List is NULL\n");
+	else if (pList->currentElementCount == 0)
+		;
+	else
+	{
+		clear = (pList->headerNode).pLink;
+		while (--(pList->currentElementCount))
+		{
+			(pList->headerNode).pLink = clear->pLink;
+			free(clear);
+			clear = (pList->headerNode).pLink;
+		}
+		(pList->headerNode).pLink = NULL;
+	}
+}
+
+/* 연결리스트 길이 반환 */
+int getLinkedListLength(LinkedList* pList)
+{
+	if (pList == NULL)
+	{
+		printf("the Array List is NULL\n");
+		return (-1);
 	}
 	return (pList->currentElementCount);
 }
-int removeLLElement(LinkedList* pList, int position)
-{
-	// if 문으로 position 값 적절한지 체크
-	// 반복문 돌려서 position 빼고 노드 연결시켜주고
-	// position에 해당하는 노드 free, NULL 처리
-}
-ListNode* getLLElement(LinkedList* pList, int position);
 
-void clearLinkedList(LinkedList* pList)
-{
-	// 뒤에서부터 free 하고 NULL 처리
-}
-int getLinkedListLength(LinkedList* pList)
-{
-	// 너무 쉬움
-}
+/* 연결리스트 삭제 */
 void deleteLinkedList(LinkedList* pList)
 {
-	// clcear 하고 pList까지 free 및 NULL 처리
+	if (pList == NULL)
+		printf("the Array List is NULL\n");
+	else
+	{
+		clearLinkedList(pList);
+		free(pList);
+		pList->currentElementCount = -1;
+	}
 }
